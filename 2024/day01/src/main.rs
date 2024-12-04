63a4be4032087148c2c150fe186a7292e266f18e
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-03 22:14:43 -0500

    restructure

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,10 +1,3 @@
-use std::fs;
-
 fn main() {
-    let loc_id_lists = fs::read("./input.txt")
-        .expect("Couldn't read file ./input.txt");
-
-    for id in loc_id_lists {
-        println!("{id}");
-    }
+    println!("Hello, world!");
 }
