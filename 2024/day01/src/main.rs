3a06e127fda5bdb74e0f6c87dd034cc4fcb6c50d
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-09 08:39:32 -0500

    remove hello world

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,8 +1,6 @@
 use std::{fs::File, io::Read};
 
 fn main() {
-    println!("Hello world!");
-
     let mut input = String::new();
     File::open("input.txt").unwrap().read_to_string(&mut input).unwrap();
 
