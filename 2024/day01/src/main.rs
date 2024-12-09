c05e9a7eee39701e0d5ceb226215962dd3465ca7
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-09 10:35:40 -0500

    split to int32 vec's

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,8 +1,16 @@
-use std::{fs::File, io::Read};
+use std::fs;
 
 fn main() {
-    let mut input = String::new();
-    File::open("input.txt").unwrap().read_to_string(&mut input).unwrap();
+    // load input file to string
+    let input = fs::read_to_string("input.txt").expect("Error reading file");
 
-    println!("{:?}", input);
+    // create integer vectors for data rows
+    let (vec1, vec2): (Vec<i32>, Vec<i32>) = input.lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            (parts[0].parse::<i32>(), parts[1].parse::<i32>())
+        }).filter_map(|(v1, v2)| match (v1, v2) {
+            (Ok(v1), Ok(v2)) => Some((v1, v2)),
+            _ => None,
+        }).unzip();
 }
