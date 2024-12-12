6d74a821b05930742763b5b630dfcf2f2f682400
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-12 17:33:37 -0500

    setup day03 test structre

---
+++ 2024/day03/src/day03.rs
@@ -0,0 +1,9 @@
+use std::{fs, path::Path};
+
+pub fn get_answer_p1(filepath: &Path) -> i32 {
+    1
+}
+
+pub fn get_answer_p2(filepath: &Path) -> i32 {
+    1
+}
--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -1,3 +1,32 @@
+mod day03;
+
+use std::path::Path;
+use day03::{get_answer_p1, get_answer_p2};
+
 fn main() {
-    println!("Hello, world!");
+    // grab input filepath
+    let project_root = env!("CARGO_MANIFEST_DIR");
+
+    println!("\n=====================Day03========================");
+    let input_path = Path::new(project_root).join("day03.txt");
+    println!("    Part 1 Answer: {}", get_answer_p1(&input_path));
+    println!("    Part 2 Answer: {}", get_answer_p2(&input_path));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let input_path = Path::new(project_root).join("day03-test.txt");
+        assert_eq!(2, get_answer_p1(&input_path));
+    }
+    #[test]
+    fn part_two() {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let input_path = Path::new(project_root).join("day03-test.txt");
+        assert_eq!(2, get_answer_p2(&input_path));
+    }
 }
