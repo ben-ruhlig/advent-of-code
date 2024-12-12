e1ada2c5a2354dd1900a9d49e4b8965797f3dd76
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-11 19:34:30 -0500

    day02 add  test outline

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -1,3 +1,19 @@
+mod day02;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        panic!();
+    }
+    #[test]
+    fn part_two() {
+        panic!();
+    }
 }
