d5506db9b6fb172abb337d165503afc24e0a185d
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-12 17:35:41 -0500

    resolve build warnings

--- 2024/day03/src/day03.rs
+++ 2024/day03/src/day03.rs
@@ -1,9 +1,11 @@
-use std::{fs, path::Path};
+use std::path::Path;
 
 pub fn get_answer_p1(filepath: &Path) -> i32 {
+    println!("{:?}", filepath);
     1
 }
 
 pub fn get_answer_p2(filepath: &Path) -> i32 {
+    println!("{:?}", filepath);
     1
 }
