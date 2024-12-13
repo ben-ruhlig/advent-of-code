bcc172ac165afeb88efa7febfef141b3fa42b63a
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-13 14:02:00 -0500

    time to day04

--- 2024/day04/src/day04.rs
+++ 2024/day04/src/day04.rs
@@ -53,8 +53,8 @@ pub fn get_answer_p1(filepath: &Path) -> i32 {
 }
 
 
+#[allow(unused)]
 pub fn get_answer_p2(filepath: &Path) -> i32 {
-    println!("{}", filepath.display());
     1
 }
 
--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -1,6 +1,6 @@
 mod day04;
 
-use std::path::Path;
+use std::{path::Path, time::Instant};
 use day04::{get_answer_p1, get_answer_p2};
 
 fn main() {
@@ -8,9 +8,15 @@ fn main() {
     let project_root = env!("CARGO_MANIFEST_DIR");
 
     println!("\n=====================Day04========================");
+    let start = Instant::now();
     let input_path = Path::new(project_root).join("day04.txt");
     println!("    Part 1 Answer: {}", get_answer_p1(&input_path));
+    let duration = start.elapsed();
+    println!("        Duration: {duration:?}");
+    let start_two = Instant::now();
     println!("    Part 2 Answer: {}", get_answer_p2(&input_path));
+    let duration_two = start_two.elapsed();
+    println!("        Duration: {duration_two:?}");
 }
 
 #[cfg(test)]
