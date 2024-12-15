d559cd83b0c9dd3241ef9dc5e2d0fdb33505b4fa
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2024-12-15 14:39:46 -0500

    spaces

--- 2024/day05/src/day05.rs
+++ 2024/day05/src/day05.rs
@@ -4,6 +4,10 @@ use std::path::Path;
 pub fn get_answer_p1(file: &str) -> i32 {
     let project_root = env!("CARGO_MANIFEST_DIR");
     let input_path = Path::new(project_root).join(file);
+    
+    // read the file content to a data structure (okay to remain as string since number is irrelevant?)
+    
+    
     1
 }
 
