8decb0a2156097f262111a60dd6a3c52d958c382
Ben <benjamin.ruhlig@gmail.com>
2024-12-24 06:06:22 -0700

    day05 p1

--- aoc_2024/src/bin/day05.rs
+++ aoc_2024/src/bin/day05.rs
@@ -1,13 +1,32 @@
-use std::{fs, path};
+use std::{
+    fs,
+    path,
+    collections::HashSet
+};
 
 #[allow(unused)]
-pub fn get_answer_p1(filepath: &path::Path) -> i32 {
-    let input = fs::read_to_string(filepath).expect("Failed to read file to string.");
+pub fn get_answer_p1(filepath: &path::Path) -> usize{
+    let input = fs::read_to_string(filepath).unwrap();
 
-    let (rules, orders) = input.split_once("\n\n").expect("Unexpected delimiter.");
-    println!("Rules:\n{}\n\nOrders:\n{}", rules, orders);
+    let (orderings, updates) = input.split_once("\n\n").unwrap();
 
-    1
+    let orderings: HashSet<(usize, usize)> = orderings
+        .lines()
+        .map(|line| (line[0..2].parse().unwrap(), line[3..].parse().unwrap()))
+        .collect();
+
+    let compare = |x: &usize, y: &usize| !orderings.contains(&(*y, *x));
+
+    let mut count = 0;
+    for update in updates.lines() {
+        let update: Vec<usize> = update.split(",").map(|x| x.parse().unwrap()).collect();
+
+        if update.is_sorted_by(compare) {
+            count += update[update.len() / 2];
+        }
+    }
+
+    count
 }
 
 #[allow(unused)]
