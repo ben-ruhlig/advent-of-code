5425c6ea6867fea9c01defa43671e81fed348d93
Ben <benjamin.ruhlig@gmail.com>
2024-12-26 12:59:21 -0700

    day0 p2 fail

--- aoc_2024/src/bin/day06.rs
+++ aoc_2024/src/bin/day06.rs
@@ -21,7 +21,7 @@ impl Direction {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Position {
+pub struct Position {
     row: usize,
     col: usize,
 }
@@ -51,26 +51,25 @@ impl GuardPosition {
             direction,
         }
     }
-
-    fn turn_right(&mut self) {
-        self.direction = self.direction.turn_right();
-    }
 }
 
 #[derive(Debug, PartialEq)]
-enum MapStatus {
-    Complete,
+enum DirectedCycle {
+    Yes,
+    No,
+}
+
+#[derive(Debug, PartialEq)]
+enum TraverseStatus {
     Incomplete,
+    Complete,
 }
 
 #[derive(Debug)]
 pub struct Map {
     boundary: Boundary,                 // map boundary
-    guard: GuardPosition,               // guard position and direction
+    start_guard: GuardPosition,         // guard position and direction
     obstacles: HashSet<Position>,       // existing obstacles
-    visited: HashSet<Position>,         // visited positions
-    new_obstacles: HashSet<Position>,   // obstacles that would create cycles
-    status: MapStatus,                  // map status complete or incomplete
 }
 
 impl Map {
@@ -85,10 +84,8 @@ impl Map {
             }
         };
 
-        let mut guard: Option<GuardPosition> = None;
+        let mut start_guard: Option<GuardPosition> = None;
         let mut obstacles: HashSet<Position> = HashSet::new();
-        let mut visited: HashSet<Position> = HashSet::new();
-        let new_obstacles: HashSet<Position> = HashSet::new();
         let mut row: usize = 0;
         let mut col: usize = 0;
         
@@ -100,141 +97,170 @@ impl Map {
                     if c == '#' {
                         obstacles.insert(Position::new(row, col));
                     } else if c == '>' || c == 'v' || c == '<' || c == '^' {
-                        guard = Some(GuardPosition::new(row, col, dir_extract(c)));
-                        visited.insert(Position::new(row, col));
+                        start_guard = Some(GuardPosition::new(row, col, dir_extract(c)));
                     }
                     col += 1;
                 });
             row += 1;
         };
 
-        // default map status is incomplete
-        let mut status = MapStatus::Incomplete;
-
-        // Helper functions
-        let inbounds = |(r, c): (usize, usize)| -> bool {
-            r < row && c < col
-        };
-        let contains_obstacle = |(r, c): (usize, usize)| -> bool {
-            obstacles.contains(&Position::new(r, c))
-        };
-        
-        // If there's a guard, check if it's surrounded by obstacles. If it is, the map is complete.
-        if let Some(g) = guard {
-            if
-                inbounds((g.position.row, g.position.col + 1)) && contains_obstacle((g.position.row, g.position.col + 1))
-                && inbounds((g.position.row + 1, g.position.col)) && contains_obstacle((g.position.row + 1, g.position.col))
-                && inbounds((g.position.row, g.position.col - 1)) && contains_obstacle((g.position.row, g.position.col - 1))
-                && inbounds((g.position.row - 1, g.position.col)) && contains_obstacle((g.position.row - 1, g.position.col))
-            {
-                status = MapStatus::Complete;
-            }
-        } else {
+        if start_guard.is_none() {
             panic!("Guard not found.");
-
         }
 
         Self {
             boundary: Boundary {row, col},
-            guard: guard.unwrap(),  // Will panic before this line if guard is None
+            start_guard: start_guard.unwrap(),  // Will panic before this line if guard is None
             obstacles,
+        }
+    }
+
+    fn next_position_valid(&self, position: &Position, direction: &Direction) -> bool {
+        if self.in_lower_bound(position, direction) {
+            let next_position = match direction {
+                Direction::Right => Position::new(position.row, position.col + 1),
+                Direction::Down => Position::new(position.row + 1, position.col),
+                Direction::Left => Position::new(position.row, position.col - 1),
+                Direction::Up => Position::new(position.row - 1, position.col),
+            };
+            self.in_upper_bound(&next_position)
+        } else {
+            false
+        }
+    }
+
+    fn in_upper_bound(&self, position: &Position) -> bool {
+        position.row < self.boundary.row && position.col < self.boundary.col
+    }
+
+    fn in_lower_bound(&self, position: &Position, direction: &Direction) -> bool {
+        !(position.row == 0 && direction == &Direction::Up
+        || position.col == 0 && direction == &Direction::Left)
+    }
+}
+
+#[derive(Debug)]
+pub struct TraversedMap {
+    visited: HashSet<Position>,
+    directed_cycle: DirectedCycle,
+}
+
+enum NextPosition {
+    DirectedCycle(DirectedCycle),
+    Position,
+}
+
+impl TraversedMap {
+    pub fn from_map(map: &Map, new_obstacle: &Option<Position>) -> Self {
+        let mut guard: GuardPosition = map.start_guard.clone();
+        let mut visited: HashSet<Position> = HashSet::new();
+        let mut directed_cycle = DirectedCycle::No;
+        let mut directed_edges: HashSet<(Position, Position)> = HashSet::new();
+        let mut status = TraverseStatus::Incomplete;
+
+        let contains_obstacle = |position: &Position| -> bool {
+            map.obstacles.contains(position)
+            || if let Some(new_obstacle) = new_obstacle {
+                new_obstacle == position
+            } else {
+                false
+            }
+        };
+
+        let get_next_position = |curr_p: &Position, d: &mut Direction| -> NextPosition {
+            let mut next_p: Option<Position> = None;
+
+            let calc_next_position = |curr_p: &Position, d: &Direction| -> Position {
+                match d {
+                    Direction::Right => Position::new(curr_p.row, curr_p.col + 1),
+                    Direction::Down => Position::new(curr_p.row + 1, curr_p.col),
+                    Direction::Left => Position::new(curr_p.row, curr_p.col - 1),
+                    Direction::Up => Position::new(curr_p.row - 1, curr_p.col),
+                }
+            };
+
+            if !map.next_position_valid(&curr_p, &d) {
+                println!("Guard is out of bounds. Complete");
+                return NextPosition::DirectedCycle(DirectedCycle::No);
+            }
+
+            let mut obs_cnt: usize = 0;
+            while next_p.is_none() && obs_cnt < 3 {
+                if map.in_lower_bound(curr_p, &d) {
+                    let potential_next_p = calc_next_position(curr_p, &d);
+                    if map.in_upper_bound(&potential_next_p) {
+                        // test for obstacle, if obstacle, turn right and update cnt
+                        if contains_obstacle(&potential_next_p) {
+                            obs_cnt += 1;
+                            d.turn_right();
+                        } else {
+                            next_p = Some(potential_next_p);
+                        }
+                    }
+                }
+            }
+
+            if obs_cnt == 3 && !next_p.is_none() {
+                println!("Directed Cycle Detected as guard is in a standstill.");
+                *dc = DirectedCycle::Yes;
+            }
+            next_p
+        };
+
+        while status == TraverseStatus::Incomplete {
+            dbg!("Current Guard: {:#?}", guard);
+            let next_p = get_next_position(&guard.position, &mut guard.direction, &mut directed_cycle);
+            if let Some(next_p) = next_p {
+                if directed_edges.contains(&(guard.position, next_p)) {
+                    println!("Directed Cycle Detected.");
+                    directed_cycle = DirectedCycle::Yes;
+                    status = TraverseStatus::Complete;
+                } else {
+                    directed_edges.insert((guard.position, next_p));
+                    visited.insert(next_p);
+                    guard.position = next_p;
+                }
+            } else {
+                dbg!("No Next Position. Complete.");
+                status = TraverseStatus::Complete;
+            }
+        }
+
+        Self {
             visited,
-            new_obstacles,
-            status,
+            directed_cycle,
         }
     }
 
     fn unique_visits(&self) -> usize {
         self.visited.len() as usize
     }
-
-    fn unique_cycle_generating_obstacles(&self) -> usize {
-        self.new_obstacles.len() as usize
-    }
-
-    fn visited(&mut self, position: Position) {
-        self.visited.insert(position);
-    }
-
-    fn complete(&mut self) {
-        self.status = MapStatus::Complete;
-    }
-
-    fn get_next_position(&self) -> Option<Position> {
-        let mut next_p: Option<Position> = None;
-        let curr_p = self.guard.position;
-        let d = self.guard.direction;
-
-        let lower_bound_issue = |curr_p: Position, d: Direction| -> bool {
-            curr_p.row == 0 && d == Direction::Up || curr_p.col == 0 && d == Direction::Left
-        };
-
-        let upper_bound_issue = |next_p: Position, b: Boundary| {
-            next_p.row > b.row  || next_p.col > b.col
-        };
-
-        let get_next_position = |curr_p: Position, d: Direction| -> Position {
-            match d {
-                Direction::Right => Position::new(curr_p.row, curr_p.col + 1),
-                Direction::Down => Position::new(curr_p.row + 1, curr_p.col),
-                Direction::Left => Position::new(curr_p.row, curr_p.col - 1),
-                Direction::Up => Position::new(curr_p.row - 1, curr_p.col),
-            }
-        };
-
-        if !lower_bound_issue(curr_p, d) {
-            let potential_next_p = get_next_position(curr_p, d);
-            if !upper_bound_issue(potential_next_p, self.boundary) {
-                next_p = Some(potential_next_p);
-            }
-        }
-        next_p
-    }
-
-    fn next_position(&mut self) {
-        let next_p = self.get_next_position();
-        if next_p.is_none() {
-            self.complete();
-        } else {
-            let next_p = next_p.unwrap();
-            if self.obstacles.contains(&next_p) {
-                self.guard.turn_right();
-                self.next_position();
-            } else {
-                self.guard.position = next_p.clone();
-                let potential_cycle_obstacle = self.get_next_position();
-                if self.visited.contains(&next_p) && !potential_cycle_obstacle.is_none() {
-                    self.new_obstacles.insert(potential_cycle_obstacle.unwrap());
-                }
-                self.visited(next_p);
-            }
-        }
-    }
-}
-
-fn get_and_process_map(filepath: &path::Path) -> Map {
-    let input = fs::read_to_string(filepath).expect("Failed to read file to string.");
-    let mut map = Map::create_from_string(&input);
-    while map.status == MapStatus::Incomplete {
-        map.next_position();
-    };
-    map
 }
 
 pub fn get_answer_p1(filepath: &path::Path) -> usize {
-    let map = get_and_process_map(filepath);
-    map.unique_visits()
+    let input = fs::read_to_string(filepath).expect("Failed to read file to string.");
+    let map = Map::create_from_string(&input);
+    let traversed_map = TraversedMap::from_map(&map, &None);
+    traversed_map.unique_visits()
 }
 
 pub fn get_answer_p2(filepath: &path::Path) -> usize {
-    let map = get_and_process_map(filepath);
-    map.unique_cycle_generating_obstacles()
+    let input = fs::read_to_string(filepath).expect("Failed to read file to string.");
+    let map = Map::create_from_string(&input);
+    let traversed_map = TraversedMap::from_map(&map, &None);
+    let mut cycle_cnt: usize = 0;
+    traversed_map.visited.into_iter().for_each(|p| {
+        if TraversedMap::from_map(&map, &Some(p)).directed_cycle == DirectedCycle::Yes {
+            cycle_cnt += 1;
+        }
+    });
+    cycle_cnt
 }
 
 fn main() {
     let filepath = path::Path::new(env!("CARGO_MANIFEST_DIR")).join("input/day06.txt");
     println!("Day06 P1: {}", get_answer_p1(&filepath));
-    // println!("Day06 P2: {}", get_answer_p2(&filepath));
+    println!("Day06 P2: {}", get_answer_p2(&filepath));
 }
 
 #[cfg(test)]
