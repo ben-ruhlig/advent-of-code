9fdb5f025b5f60c986065c984a1165c89f5b9b16
Ben <benjamin.ruhlig@gmail.com>
2025-01-04 19:21:49 -0700

    readline

--- aoc_2024/src/problems/day7p1.rs
+++ aoc_2024/src/problems/day7p1.rs
@@ -1,7 +1,10 @@
 use std::fs;
 
 
-pub fn solution(file_path: &str) -> i32 {
+pub fn solution(file_path: &str) -> u32 {
     let input = fs::read_to_string(file_path).unwrap();
+    input.lines().for_each(|line| {
+        println!("{}", line);
+    });
     1
 }
